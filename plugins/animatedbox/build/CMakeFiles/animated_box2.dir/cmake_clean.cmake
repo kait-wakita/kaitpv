cfdef31c9bdd0edf047b130f589514ed6f9754ea
Toshihiro Wakita <wakita@cco.kanagawa-it.ac.jp>
2023-01-08 20:09:01 +0900

    add 2nd pedestrian

---
+++ plugins/animatedbox/build/CMakeFiles/animated_box2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/animated_box2.dir/animated_box2.cc.o"
+  "libanimated_box2.pdb"
+  "libanimated_box2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/animated_box2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
